a54b61e6919d8ddc9b9192bbb0ff5643da074601
Maria Claudia <mariaclaudiapanclas@gmail.com>
2023-08-20 15:36:48 -0500

    Cambios realizados

--- Principal.java
+++ Principal.java
@@ -1,12 +1,22 @@
-public class Suma_de_Numeros_Pares {
+import java.util.Scanner;
+
+public class Principal {
     public static void main(String[] args) {
-        int limite = 20;
-        int suma = 0;
-
-        for (int i = 2; i <= limite; i += 2) {
-            suma += i;
+        Scanner scanner = new Scanner(System.in);
+        int numeroAdivinar = 25;
+        
+        System.out.println("¡Bienvenido al juego de adivina el numero!");
+        System.out.print("Introduce tu suposicion: ");
+        int suposicion = scanner.nextInt();
+        
+        if (suposicion == numeroAdivinar) {
+            String premio = "EL PREMIO";
+            System.out.println("¡Bien! ¡Has adivinado el número y ganaste un " + premio + "!");
+        } else {
+            System.out.println("Lo siento, no adivinaste el numero.");
         }
-
-        System.out.println("La suma de los números pares hasta " + limite + " es: " + suma);
+        
+        scanner.close();
     }
 }
+
